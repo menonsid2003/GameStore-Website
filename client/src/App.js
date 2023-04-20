0c24c1b8c78d169e82a004a032fa9fb0aa2d2dd6
Sidharth Menon <menonsid2003@gmail.com>
2023-04-20 01:56:13 -0400

    working css

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,76 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet, } from "react-router-dom";
+//BrowserRouter, Routes, Route, 
 import Inventory from "./pages/Inventory";
-import Add from "./pages/Add";
-import Update from "./pages/Update";
+//import Add from "./pages/Add";
+//import Update from "./pages/Update";
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Single from './pages/Single';
+import "./style.scss";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: "/game/:id",
+        element: <Single />
+      },
+      {
+        path: '/inventory',
+        element: <Inventory />
+      },
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <div className='container'>
+        <RouterProvider router={router} />
+      </div>
+    </div>
+  );
+};
+
+
+
+export default App;
+
+/*
+<BrowserRouter>
         <Routes>
           <Route path="/" element={<Inventory />} />
           <Route path="/add" element={<Add />} />
           <Route path="/update" element={<Update />} />
         </Routes>
       </BrowserRouter>
-    </div>
-  );
-}
-
-export default App;
+ */
