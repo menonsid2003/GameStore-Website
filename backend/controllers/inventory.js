babda1d7443a5e4dc9c214f5285f1aa2dc2e5513
Sidharth Menon <menonsid2003@gmail.com>
2023-04-23 15:50:25 -0400

    library work transfer

--- backend/controllers/inventory.js
+++ backend/controllers/inventory.js
@@ -17,6 +17,32 @@ export const getGame = (req, res) => {
 };
 
 export const addGame = (req, res) => {
+    //check if game already exists
+    const q = "SELECT * FROM video_game WHERE SKU = ?"
+
+    db.query(q, [req.body.SKU], (err, data) => {
+        if (err) return res.json(err)
+        if (data.length) return res.status(409).json("SKU already exists!");
+
+        const q = "INSERT INTO video_game(`sku`, `title`, `year_of_release`, `genre1`, `genre2`, `systemID`, `rating`, `price`, `publisherID`, `cover`) VALUES (?)"
+        const values = [
+            req.SKU,
+            req.title,
+            req.year,
+            req.genre1,
+            req.genre2,
+            req.system,
+            req.rating,
+            req.price,
+            req.publisher,
+            req.cover,
+        ]
+
+        db.query(q, [values], (err, data) => {
+            if (err) return res.json(err)
+            return res.status(200).json("Game has been added.")
+        })
+    });
 };
 
 export const deleteGame = (req, res) => {
--- backend/routes/inventory.js
+++ backend/routes/inventory.js
@@ -1,13 +1,12 @@
 import express from "express";
-import { deleteGame, getGame, getGames, updateGame } from "../controllers/inventory.js";
+import { addGame, deleteGame, getGame, getGames, updateGame } from "../controllers/inventory.js";
 
 const router = express.Router()
 
 router.get("/", getGames)
 router.get("/:sku", getGame)
-router.post("/",)
+router.post("/add", addGame)
 router.delete("/:sku", deleteGame)
 router.put("/:sku", updateGame)
-
 
 export default router
